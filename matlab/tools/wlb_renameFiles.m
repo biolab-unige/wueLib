876b7af08148881372b1ae83078f2847d4b6419c
andrea.canessa <andrea.canessa83@gmail.com>
2015-06-18 15:41:04 +0200

    TOOLS: Update renaming script to support PCS and EMG files

--- matlab/tools/wlb_renameFiles.m
+++ matlab/tools/wlb_renameFiles.m
@@ -1,27 +1,44 @@
 
-[subjectNames, experiments, drug, trial, fileNames] = textread('PCSgaitIniList.csv','%s %s %s %d %s\n',...
-																										'delimiter',',','headerlines',1);
+[csv_match_file,csv_match_file_path] = uigetfile('/opt/data/*.csv','Select the csv matching file');
 
-fid = fopen('./conversion.log','w');
-for file = 1:numel(fileNames)
+[subjectNames, experiments, drug, trial, fileNames_pcs,fileNames_emg] = textread(fullfile(csv_match_file_path,csv_match_file),...
+    '%s %s %s %d %s %s\n','delimiter',',','headerlines',1);
 
-		newFileName_ = strjoin([subjectNames(file),experiments(file),...
-													drug(file),{strcat('trial',num2str(trial(file)))}],'_');
-		newFileName_ = regexprep(newFileName_,'\s','');
-		oldFileName_ = fullfile(lower(subjectNames{file}),fileNames{file});
-		newFileName_ = fullfile(lower(subjectNames{file}),newFileName_);
+fid = fopen(fullfile(csv_match_file_path,'conversion.log'),'w');
+
+for file = 1:numel(fileNames_pcs)
+
+		newFileName_pcs = strjoin([subjectNames(file),experiments(file),...
+													drug(file),{strcat('trial',num2str(trial(file)))},'pcs'],'_');
+		oldFileName_ = fullfile(lower(subjectNames{file}),fileNames_pcs{file});
+		newFileName_pcs = fullfile(lower(subjectNames{file}),newFileName_pcs);
 
 		extension = {'.txt','.xml'};
 
-		for ext = 1:2
+		for ext = 1:numel(extension)
 
 				oldFileName = strcat(oldFileName_,extension{ext});
-				newFileName = strcat(newFileName_,extension{ext});
-				[s, mesg] = movefile(oldFileName,newFileName);
+				newFileName = strcat(newFileName_pcs,extension{ext});
+				[s, mesg] = movefile(fullfile(csv_match_file_path,oldFileName),...
+                    fullfile(csv_match_file_path,newFileName));
 				if(~s)
 						fprintf(fid,mesg);
 				end
 
-		end
+        end
+        
+        newFileName_emg= strjoin([subjectNames(file),experiments(file),...
+													drug(file),{strcat('trial',num2str(trial(file)))},'emg'],'_');
+		oldFileName_ = fullfile(lower(subjectNames{file}),fileNames_emg{file});
+		newFileName_emg = fullfile(lower(subjectNames{file}),newFileName_emg);
+
+        oldFileName = strcat(oldFileName_,'.txt');
+        newFileName = strcat(newFileName_emg,'.txt');
+				[s, mesg] = movefile(fullfile(csv_match_file_path,oldFileName),...
+                    fullfile(csv_match_file_path,newFileName));
+        if(~s)
+                fprintf(fid,mesg);
+        end
+
 end
 fclose(fid);
